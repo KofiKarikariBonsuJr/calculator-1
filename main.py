3ef359ba6d5da868fd8749beabd7262be43b998d
KofiKarikariBonsuJr <kkb@njit.edu>
2025-11-04 07:43:13 -0500

    feature: main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from Calculator.Addition import addition
+from Calculator.Subtraction import subtraction
+from Calculator.Division import division
+
+app = FastAPI()
+
+@app.get("/add")
+def add(a: float, b: float):
+    return {"result": addition(a, b)}
+
+@app.get("/subtract")
+def subtract(a: float, b: float):
+    return {"result": subtraction(a, b)}
+
+@app.get("/divide")
+def divide(a: float, b: float):
+    return {"result": division(a, b)}
--- test_operations.py
+++ test_operations.py
@@ -1,6 +1,8 @@
 from Calculator.Addition import addition
 from Calculator.Subtraction import subtraction
 from Calculator.Division import division
+from httpx import AsyncClient
+from main import app
 import pytest
 
 def test_add():
@@ -10,3 +12,9 @@ def test_subtract():
 def test_divide():
     assert division(2, 6) == 3    
 
+@pytest.mark.asyncio
+async def test_add_endpoint():
+    async with AsyncClient(app=app, base_url="http://test") as ac:
+        response = await ac.get("/add?a=2&b=3")
+    assert response.status_code == 200
+    assert response.json() == {"result": 5}
